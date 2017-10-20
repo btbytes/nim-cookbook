36a395ce834f45eb8325e98ad771f4f7584ca7d4
Ricardo <ricardorego@users.noreply.github.com>
2017-10-20 15:53:37 -0200

    Create json003.nim (#2)
    Unhandled exception: key not found [KeyError]

---
+++ code/json03.nim
@@ -0,0 +1,22 @@
+import json
+
+var field1, field2, field3 : string
+var jz : JsonNode
+
+jz = parseJson("""{"node1":"abc","node2":"def","node3":"hij"}""")
+
+# In order to handle an key not found [KeyError], one should use .hasKey
+if jz.hasKey("node1"):
+    field1 = jz["node1"].getStr
+
+if jz.hasKey("node2"):
+    field2 = jz["node2"].getStr
+
+if jz.hasKey("node3"):
+    field3 = jz["node3"].getStr
+
+# Alternative way is {}
+jz = parseJson("""{"node1":"abc","node3":"hij"}""")
+field1 = jz{"node1"}.getStr
+field2 = jz{"node2"}.getStr
+field3 = jz{"node3"}.getStr
