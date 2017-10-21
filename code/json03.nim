28c5a08120e4304523a881e34b1b178800ddfde3
Daniil Yarancev <21169548+Yardanico@users.noreply.github.com>
2017-10-21 18:52:37 +0300

    Update json03.nim

--- code/json03.nim
+++ code/json03.nim
@@ -1,22 +1,22 @@
 import json
 
-var field1, field2, field3 : string
-var jz : JsonNode
+var field1, field2, field3: string
+var data: JsonNode
 
-jz = parseJson("""{"node1":"abc","node2":"def","node3":"hij"}""")
+data = parseJson("""{"node1":"abc","node2":"def","node3":"hij"}""")
 
-# In order to avoid a possible key not found [KeyError], one should use .hasKey
-if jz.hasKey("node1"):
-    field1 = jz["node1"].getStr
+# We should check if JSON has a field to avoid KeyError
+if "node1" in data:
+  field1 = data["node1"].getStr()
 
-if jz.hasKey("node2"):
-    field2 = jz["node2"].getStr
+if "node1" in data:
+  field2 = data["node2"].getStr()
 
-if jz.hasKey("node3"):
-    field3 = jz["node3"].getStr
+if "node3" in data:
+  field3 = data["node3"].getStr()
 
-# Alternative way is {}
-jz = parseJson("""{"node1":"abc","node3":"hij"}""")
-field1 = jz{"node1"}.getStr
-field2 = jz{"node2"}.getStr
-field3 = jz{"node3"}.getStr
+# Alternative way is {} - if key is not found, it will return default value
+data = parseJson("""{"node1":"abc","node3":"hij"}""")
+field1 = data{"node1"}.getStr()
+field2 = data{"node2"}.getStr()
+field3 = data{"node3"}.getStr()
